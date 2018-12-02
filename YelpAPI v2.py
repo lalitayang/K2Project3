e62d07ba827d9c66102c3cd80f73c66c06a92ed9
lalitayang <32180327+lalitayang@users.noreply.github.com>
2018-12-02 11:10:52 -0800

    Add files via upload

--- YelpAPI v2.py
+++ YelpAPI v2.py
@@ -7,7 +7,7 @@ Created on Sun Nov 25 13:43:56 2018
 """
 
 import os
-os.chdir('/Users/lalitayang/Documents/K2/Unit 3/Project/')
+os.chdir(r"C:\Users\lalyang\Documents\GitHub\K2Project3")
 print(os.getcwd())
 
 all_friends = load_obj('all_friends')
@@ -24,7 +24,9 @@ my_review_df = load_obj('my_review_df' )
 friend_reviews = load_obj('friends_reviews' )
 friend_rev_df = load_obj('friend_rev_df' )
 
-details = ['alias', 'is_closed', 'price', 'rating', 'review_count']
+master_biz_ids = load_obj('master_biz_ids')
+
+details = ['alias', 'is_closed', 'price', 'rating', 'review_count', 'id']
 
 ###############################################################################
 ##################### concat all dfs
@@ -34,44 +36,62 @@ master_biz = pd.concat([my_bm_df, my_review_df, friend_bm_df, friend_rev_df])
 master_biz_ids = list(set(master_biz['business_id']))
 # 16,742 total unique businesses
 
+master_biz_ids.index('robin-san-francisco')
+
+save_obj(master_biz_ids, 'master_biz_ids')
 ###############################################################################
 ##################### call business details endpoint for ALL BUSINESSES
 ##################### Yelp API only allows for 5000 API calls a day
 ###############################################################################
 
-master_bizdet_json_4999 = []
+master_biz_ids = load_obj('master_biz_ids')
 
-for business in master_biz_ids[:5000]:
-    df = get_biz_details(api_key, business)
+#master_bizdet_json_4999 = []
+
+for business in master_biz_ids[1000:5000]:
+    df = get_biz_details(CY_api, business)
     master_bizdet_json_4999.append(df)
 
 save_obj(master_bizdet_json_4999, 'master_bizdet_json_4999')
     
-master_bizdet_json_9999 = []
+#master_bizdet_json_9999 = []
 
-for business in master_biz_ids[5000:10000]:
-    df = get_biz_details(api_key, business)
+for business in master_biz_ids[5000:10000]: 
+    df = get_biz_details(JW_api, business)
     master_bizdet_json_9999.append(df)
 
 save_obj(master_bizdet_json_9999, 'master_bizdet_json_9999')
 
-master_bizdet_json_14999 = []
+#master_bizdet_json_14999 = []
 
-for business in master_biz_ids[10000:15000]: # still need to run
+for business in master_biz_ids[10000:15000]:
     df = get_biz_details(api_key, business)
     master_bizdet_json_14999.append(df) 
-    
-master_bizdet_json_16742 = []
+
+save_obj(master_bizdet_json_14999, 'master_bizdet_json_14999')
+
+#master_bizdet_json_16742 = []
 
 for business in master_biz_ids[15000:]: # still need to run
-    df = get_biz_details(api_key, business)
+    df = get_biz_details(PS_api, business)
     master_bizdet_json_16742.append(df) 
+    
+save_obj(master_bizdet_json_16742, 'master_bizdet_json_16742')
+        
+master_bizdet_json_16742 = master_bizdet_json_16742[:1271]       
+
+test = get_biz_details(JW_api, 'robin-san-francisco') 
 
 ###############################################################################
 ##################### concat all jsons and convert to 1 master dataframe; 
 ##################### drop duplicates to ensure no duplciates
 ###############################################################################
     
+master_bizdet_json_4999 = load_obj('master_bizdet_json_4999')    
+master_bizdet_json_9999 = load_obj('master_bizdet_json_9999')
+master_bizdet_json_14999 = load_obj('master_bizdet_json_14999')
+master_bizdet_json_16742 = load_obj('master_bizdet_json_16742')
+
 master_bizdet_listdf = []
 
 for business in master_bizdet_json_4999:
@@ -79,13 +99,25 @@ for business in master_bizdet_json_4999:
 
 for business in master_bizdet_json_9999:
     master_bizdet_listdf.append(json_to_df(business, details))
+  
+for business in master_bizdet_json_14999:
+    master_bizdet_listdf.append(json_to_df(business, details))
     
-### need to append 10000+
+for business in master_bizdet_json_16742:
+    master_bizdet_listdf.append(json_to_df(business, details))
+
+### need to append 15000+
     
 master_bizdet_df = pd.concat(master_bizdet_listdf)
-master_bizdet_df.drop_duplicates() 
+master_bizdet_df.drop_duplicates(subset='alias', keep='first', inplace=True) 
 
 save_obj(master_bizdet_df, 'master_bizdet_df')
+master_bizdet_df = load_obj('master_bizdet_df')
+
+master_bizdet_df[master_bizdet_df['alias'].isnull()]
+
+master_bizdet_df = master_bizdet_df.reset_index()
+master_biz
 
 ###############################################################################
 ##################### left join with review and bookmark dataframes to get 
@@ -94,10 +126,62 @@ save_obj(master_bizdet_df, 'master_bizdet_df')
 
 # need to run the below
 
+for df in [my_bm_df, my_review_df, friend_rev_df, friend_bm_df]:
+    df.rename(columns={'id':'user_id'}, inplace=True)
+    
+for df in [my_review_df, friend_rev_df]:
+    df.rename(columns={'rating':'user_rating'}, inplace=True)
+
 my_bm_df2 = pd.merge(my_bm_df, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
 
 my_review_df2 = pd.merge(my_review_df, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
 
-friend_rev_df2 = pd.merge(friend_rev_df2, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
+friend_rev_df2 = pd.merge(friend_rev_df, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
 
-friend_bm_df2 = pd.merge(friend_bm_df2, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
+friend_bm_df2 = pd.merge(friend_bm_df, master_bizdet_df, how='left', left_on = 'business_id', right_on='alias')
+
+b1 = my_bm_df2[my_bm_df2['alias'].isnull()]
+b2 = my_review_df2[my_review_df2['alias'].isnull()]
+b3 = friend_rev_df2[friend_rev_df2['alias'].isnull()]
+b4 = friend_bm_df2[friend_bm_df2['alias'].isnull()]
+
+blanks = pd.concat([b1, b2, b3, b4])
+
+
+
+
+
+
+
+s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
